b1ede1711bc70adfcce1e0d5881abc66405974a8
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2022-10-20 13:55:53 +0200

    firefox-av-tc13: add TC 13.1 version of firefox-av
    This version does not support vbox guest additions.

---
+++ appliances/firefox-av-tc13/iso.mk
@@ -0,0 +1,11 @@
+BASE_ISO       := CorePure64-13.1.iso
+VMLINUZ        := vmlinuz64
+CORE_GZ        := 13.1/tinycorepure64.gz
+APPS(DOWNLOAD) := 13.x/Xorg-7.7 \
+                  13.x/pavucontrol \
+                  13.x/i3 \
+                  13.x/aterm \
+                  13.x/graphics-5.15.10-tinycore64
+APPS(ARCHIVE)  := 13.x/firefox
+APPS(SHARE)    := 13.x/audio-vbox \
+                  13.x/uvcvideo
