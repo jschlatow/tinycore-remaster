595b8224de1e50a56b334a69932784ad5bbc4d4d
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2025-04-29 11:33:03 +0200

    add keepass VM

---
+++ appliances/keepass-tc16/iso.mk
@@ -0,0 +1,10 @@
+BASE_ISO       := CorePure64-16.0.iso
+VMLINUZ        := vmlinuz64
+CORE_GZ        := 16.0/tinycorepure64.gz
+EXTRA_INITRDS  := 16.x/vboxga_tools.gz
+APPS(DOWNLOAD) := 16.x/Xorg-7.7 \
+                  16.x/i3 \
+                  16.x/aterm \
+                  16.x/graphics-6.12.11-tinycore64 \
+                  16.x/keepassx
+APPS(ARCHIVE)  := 16.x/vboxclient
