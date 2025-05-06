b3db99e9997abd19258ebe244e0a63d8f043656e
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2025-05-06 16:54:58 +0200

    keepass: replace KeePassX with KeePassXC

--- appliances/keepass-tc16/iso.mk
+++ appliances/keepass-tc16/iso.mk
@@ -5,6 +5,6 @@ EXTRA_INITRDS  := 16.x/vboxga_tools.gz
 APPS(DOWNLOAD) := 16.x/Xorg-7.7 \
                   16.x/i3 \
                   16.x/aterm \
-                  16.x/graphics-6.12.11-tinycore64 \
-                  16.x/keepassx
+                  16.x/graphics-6.12.11-tinycore64
+APPS(SHARE)    := 16.x/keepassxc
 APPS(ARCHIVE)  := 16.x/vboxclient
