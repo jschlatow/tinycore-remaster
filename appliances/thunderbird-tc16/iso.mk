c90bffd8da98b6852cdd230e962c0aa25081b25e
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2025-05-07 16:39:53 +0200

    update thunderbird

--- appliances/thunderbird-tc16/iso.mk
+++ appliances/thunderbird-tc16/iso.mk
@@ -5,6 +5,6 @@ EXTRA_INITRDS  := 16.x/vboxga_tools.gz
 APPS(DOWNLOAD) := 16.x/Xorg-7.7 \
                   16.x/i3 \
                   16.x/aterm \
-                  16.x/thunderbird \
                   16.x/graphics-6.12.11-tinycore64
+APPS(SHARE)    := 16.x/thunderbird
 APPS(ARCHIVE)  := 16.x/vboxclient
